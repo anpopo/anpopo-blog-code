f880c9f4ebdddcb3f807eaf69ab91b6ead0f0ee4
ansehyeong <dkstpgud@gmail.com>
2022-05-29 01:19:10 +0900

    # [feat] 단일 책임 원칙 / 응집도 높은 코드 작성
    - 단일 책임 원칙을 따라 하나의 기능을 하는 클래스로 기능을 나눈다. 또한 해당 클래스가 바뀌어야 하는 이유는 오직 하나이다.
    
    - 응집도 (cohesion) 는 클래스나 메서드의 책임이 서로 어떻게 관련되어 있는가를 나타내는 지표로 응집도 높은 코드가 유지보수 하기 좋다. 여러 분류 기준이 있지만 연관되어 있는 클래스 혹은 메서드의 책임과 역할에 대해 생각하고 기준에 맞게 분리하자.

---
+++ basic-of-javastudy/src/main/java/basic/of/javastudy/banksystem/BankTransactionAnalyzer.java
@@ -0,0 +1,40 @@
+package basic.of.javastudy.banksystem;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 단순히 csv 파일의 모든 거래내역의 합을 구함.
+ * 파일이 비어있거나, 1번 인덱스에 대해 파싱을 못한다거나, 데이터가 완벽하지 않아 인덱스에 문제가 있다거나 하는 예외적인 처리는 안함.
+ */
+public class BankTransactionAnalyzer {
+
+    private static final String RESOURCE = "C:/Users/dkstp/Projects/anpopo-blog-code/basic-of-javastudy/src/main/resources/";
+    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+
+    public static void main(String[] args) throws IOException {
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCE + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+
+        collectSummary(bankStatementProcessor);
+    }
+
+    private static void collectSummary(BankStatementProcessor bankStatementProcessor) {
+        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total for all transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
+        System.out.println("The total for all transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
+        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }
+
+
+}
