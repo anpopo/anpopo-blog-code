1450e9fe8869ecc0161998e2df766e398c2f9536
ansehyeong <dkstpgud@gmail.com>
2022-06-01 22:19:30 +0900

    # [feat] 자바 기본 - tcp 연결에서 ServerSocket 과 Socket 을 이용해서 간단한 chatting example 구현

---
+++ basic-of-javastudy/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module anpopo.blog.code.basic.of.javastudy.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires lombok;
+    requires java.xml.crypto;
+
+    opens basic.of.javastudy.networkpractice;
+}
