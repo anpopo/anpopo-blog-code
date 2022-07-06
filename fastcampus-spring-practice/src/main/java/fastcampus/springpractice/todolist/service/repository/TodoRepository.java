76993e42d982b5c31f44f49bf87788d4b7444629
ansehyeong <dkstpgud@gmail.com>
2022-07-06 11:25:04 +0900

    # master [chore] package 정리 및 프로젝트 모듈 정리

--- fastcampus-spring-practice/src/main/java/fastcampus/springpractice/todolist/service/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package fastcampus.springpractice.todolist.service.repository;
-
-import fastcampus.springpractice.todolist.model.TodoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<TodoModel, Long> {
-}
